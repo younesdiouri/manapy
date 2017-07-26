851e598d861b5c5af0412e6c56456a4c331652a3
younesdiouri <younesdiouri@gmail.com>
2017-07-26 19:58:18 +0200

    django initialised

---
+++ manapy/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    text = """<h1>Bienvenue sur mon blog !</h1>
+              <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
+    return HttpResponse(text)
